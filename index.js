d48ed3c56cecd1bf460dd44d2755412b8ed67933
Qmo <016600@senao.com.tw>
2017-12-29 17:36:07 +0800

    #comment hw1

--- index.js
+++ index.js
@@ -1,60 +1,51 @@
 var clock = {
-
-  digits : ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
-
-  init : function(){
-    this.hour = document.getElementsByClassName('hour');
-    this.minute = document.getElementsByClassName('minute');
-    this.second = document.getElementsByClassName('second');
-
-    this.drawInterval(this.drawSecond, function(time){
-      return 1000 - time[3];
-    })
-
-    this.drawInterval(this.drawMinute, function(time){
-      return 60000 - time[2] * 1000 - time[3];
-    })
-
-
-    this.drawInterval(this.drawHour, function(time){
-      return (60 - time[1]) * 60000 - time[2] * 1000 - time[3];
-    })
+  getTimeArray: function() {
+    var date = new Date();
+    return [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+      date.getMilliseconds()];
   },
 
-  getTimeArray : function(){
-    var dat = new Date();
-    return [dat.getHours(), dat.getMinutes(), dat.getSeconds(), dat.getMilliseconds()];
+  onTick: function() {
+    setInterval(function() { observable.update(clock.getTimeArray()); }, 1000);
+  },
+};
+
+var observable = {
+  update: function(time) {
+    digitalClock.init(time);
+  },
+};
+
+var digitalClock = {
+
+  digits: [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'],
+
+  init: function(time) {
+    this.drawDigits(document.getElementsByClassName('hour'), time[0]);
+    this.drawDigits(document.getElementsByClassName('minute'), time[1]);
+    this.drawDigits(document.getElementsByClassName('second'), time[2]);
   },
 
-  drawInterval : function(func, timeCallback){
-    var time = this.getTimeArray();
-
-    func.call(this, time);
-
-    var that = this;
-    setTimeout(function(){
-      that.drawInterval(func, timeCallback);
-    }, timeCallback(time));
-  },
-
-  drawHour : function(time){
-    this.drawDigits(this.hour, time[0]);
-  },
-
-  drawMinute : function(time){
-    this.drawDigits(this.minute,  time[1]);
-  },
-
-  drawSecond : function(time){
-    this.drawDigits(this.second,  time[2]);
-  },
-
-  drawDigits : function(element, digit){
+  drawDigits: function(element, digit) {
     var ten = Math.floor(digit / 10);
     var one = Math.floor(digit % 10);
 
-    element[0].innerHTML = '<span class="digit ' + this.digits[ten] + '"></span><span class="digit ' + this.digits[one] + '"></span>';
-  }
+    element[0].innerHTML = '<span class="digit ' + this.digits[ten] +
+        '"></span><span class="digit ' + this.digits[one] + '"></span>';
+  },
 };
 
-clock.init();
+clock.onTick();
